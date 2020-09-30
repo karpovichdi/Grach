95add63c03ad63fbd6d7169728e5cb0dccb80b45
mobileconductor <karpovichdi@hotmail.com>
2020-09-30 15:26:07 +0300

    Added: AppCenter sdk integration.

--- Grach/Grach/Grach/App.xaml.cs
+++ Grach/Grach/Grach/App.xaml.cs
@@ -3,6 +3,9 @@ using Grach.Extensions;
 using Grach.Pages;
 using Grach.Services;
 using Grach.ViewModels;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Prism;
 using Prism.Common;
 using Prism.Ioc;
@@ -36,6 +39,13 @@ namespace Grach
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             AppDependencies.Register(containerRegistry, Container);
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=00a649e6-a0a2-4f4d-b759-b967f0da9778;" +
+                            "ios=bc8dc21e-37e1-435c-ac44-128eddda000c;",
+                typeof(Analytics), typeof(Crashes));
         }
     }
 }
