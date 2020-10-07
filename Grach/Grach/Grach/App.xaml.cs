269950195c357d56b9b13dc58545466729d321b6
mobileconductor <karpovichdi@hotmail.com>
2020-10-07 11:32:59 +0300

    Added:  second app center app.

--- Grach/Grach/Grach/App.xaml.cs
+++ Grach/Grach/Grach/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Grach.Core.Interfaces;
+﻿using Grach.Core.Enums;
+using Grach.Core.Helpers;
+using Grach.Core.Interfaces;
 using Grach.Extensions;
 using Grach.Pages;
 using Grach.Services;
@@ -43,9 +45,19 @@ namespace Grach
 
         protected override void OnStart()
         {
-            AppCenter.Start("android=00a649e6-a0a2-4f4d-b759-b967f0da9778;" +
-                            "ios=bc8dc21e-37e1-435c-ac44-128eddda000c;",
-                typeof(Analytics), typeof(Crashes));
+            if (CompilerFlagHelper.CompilerDirectives.HasFlag(CompilerDirectives.RELEASE2))
+            { 
+                AppCenter.Start("android=00a649e6-a0a2-4f4d-b759-b967f0da9778;" +
+                                "ios=bc8dc21e-37e1-435c-ac44-128eddda000c;",
+                    typeof(Analytics), typeof(Crashes));
+            }
+            
+            if (CompilerFlagHelper.CompilerDirectives.HasFlag(CompilerDirectives.RELEASE))
+            { 
+                AppCenter.Start("android=8eb6a825-d938-4901-a56f-fcb6cd695d3f;" +
+                                "ios=43bc1e3a-03c6-4289-ab08-da069db424fd;",
+                    typeof(Analytics), typeof(Crashes));
+            }
         }
     }
 }
